3eb0a23facb0655ddce3f86de18d0ad52fbffc88
HasanovDoc <hasanov326@mail.ru>
2023-06-23 04:34:20 +0400

    fix, add close popUp button

--- js/importGraph.js
+++ js/importGraph.js
@@ -2,6 +2,7 @@ const importBtn = document.querySelector('.importGraph');
 const popUp = document.querySelector('.popUp__container');
 const btnSendJson = document.querySelector('.btn-sendJson');
 const inputJsonFile = document.querySelector('.inputJsonFile');
+const closePopUp = document.querySelector('.popUp__close');
 
 //Вызов popUm меню
 importBtn.addEventListener('click', () => {
@@ -9,9 +10,9 @@ importBtn.addEventListener('click', () => {
 });
 
 //Закрытие popUp
-// popUp.addEventListener('click', () => {
-//     popUp.classList.remove('visible');
-// });
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('visible');
+});
 
 //Получение Json
 btnSendJson.addEventListener('click', () => {
@@ -45,13 +46,13 @@ btnSendJson.addEventListener('click', () => {
 
 let json = {
     "Node-0": {
-        "target": [3]
+        "target": [1]
     },
     "Node-1": {
-        "target": [0, 2, 3]
+        "target": [0, 2]
     },
     "Node-2": {
-        "target": [3]
+        "target": []
     },
     "Node-3": {
         "target": []
@@ -61,33 +62,22 @@ let json = {
 function drawGraphByJson(jsonObj) {
     let x_0 = canvas.width / 2;
     let y_0 = canvas.height / 2;
-    let r = (canvas.height / 2) - 30;
+    let r = y_0 - 100;
     let countNode = Object.keys(jsonObj).length;
     let iter = 0;
+
     for (let el in jsonObj) {
         let label = el;
+
         let XX = (x_0 + (r * Math.cos(2 * Math.PI * iter / countNode)));
         let YY = (y_0 + (r * Math.sin(2 * Math.PI * iter / countNode)));
         addNodesC(Nods, iter, XX, YY, label);
-        drawNode(Nods[iter].X, Nods[iter].Y, Nods[iter].label);
-        iter++;
 
-        //console.log(jsonObj[el].target);
-
-    }
-    iter = 0;
-    for (let el in jsonObj) {
-        if (jsonObj[el].target.length != 0) {
-            for (let trg in jsonObj[el].target) {
-                //console.log(jsonObj[el].target[trg]);
-
-                addEdgs(iter, jsonObj[el].target[trg]);
-                drawEdges(Nods[iter].X, Nods[iter].Y, Nods[jsonObj[el].target[trg]].X, Nods[jsonObj[el].target[trg]].Y);
-
-            }
-
-
+        for (let trg in jsonObj[el].target) {
+            addEdgs(iter, jsonObj[el].target[trg]);
         }
-        iter++
+
+        iter++;
     }
+    redrawGraph();
 }
